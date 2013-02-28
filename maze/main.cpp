26cb85b78dbab2756357277c681c43e3eab5f762
Kaushalya <kaushalya.kularatnam@lbi.com>
2013-02-28 12:54:10 +0000

    Initial Commit

---
+++ maze/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  maze
+//
+//  Created by Kaushalya Kularatnam on 28/02/2013.
+//  Copyright (c) 2013 LBi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
